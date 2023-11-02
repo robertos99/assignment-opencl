6a87200ec208ea28545eb6cf14d7d19f47320c1e
Robert <games.robert2@t-online.de>
2023-11-02 22:51:13 +0100

    dk

--- tests/test_helloworld.cpp
+++ tests/test_helloworld.cpp
@@ -1,4 +1,3 @@
-// This tells Catch to provide a main() - only do this in one cpp file
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
@@ -9,6 +8,5 @@ TEST_CASE("say_hello returns the correct string", "[hello_world]")
     const char *expected = "Hello, World!";
     const char *actual = say_hello();
 
-    // The REQUIRE macro in Catch2 is used for expressing assertions.
     REQUIRE(std::string(actual) == std::string(expected));
 }
