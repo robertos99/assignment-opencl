a38c0b81efa3494821623806c891817bbe888e7f
binh <dang.trinh@student.htw-berlin.de>
2023-12-04 16:18:41 +0100

    changed test files

---
+++ tests/test.cpp
@@ -0,0 +1,19 @@
+#include "openmp_lib.hpp"
+
+void setUp(void)
+{
+    // This function runs before EACH TEST
+}
+
+void tearDown(void)
+{
+    // This function runs after EACH TEST
+}
+
+void some_test_method_name(void)
+{
+}
+
+int main(void)
+{
+}
